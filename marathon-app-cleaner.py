573cfc9ac78f1124fa57c63d9ee39743d3148134
Vivek Juneja <vivekjuneja@Viveks-MacBook-Pro.local>
2015-12-13 14:31:06 +0900

    Comments added. Fixed bug with URL for Marathon Groups GET

--- marathon-app-cleaner.py
+++ marathon-app-cleaner.py
@@ -1,3 +1,13 @@
+# Application Name	: marathon-app-cleaner.py
+# Author			: Vivek Juneja
+# Created			: 11th December 2015
+# Last Modified	:
+# Version			: 1.0
+
+
+# Description		: Removes Old applications deployed on Apache Marathon. This is used to create ephemeral applications that are removed after a configurable duration
+
+
 from sys import argv
 import re
 from sets import Set
@@ -8,9 +18,9 @@ import datetime
 from pytz import timezone
 import argparse
 
-only_use_groups=True
-avoid_label_key=None
-avoid_label_value=None
+only_use_groups=True    # Only delete Applicaton Groups
+avoid_label_key=None    # The Marathon Label Key to be used to ignore application groups
+avoid_label_value=None  # The Marathon Label Value for the Key to be used to ignore application groups
 
 '''
 Get all the app groups with their version timestamps
@@ -18,37 +28,40 @@ Get all the app groups with their version timestamps
 marathon_endpoint - http endpoint of marathon with port number
 avoid_label_key - marathon application label "key" that is used to filter out the results
 avoid_label_value - value of the "key" that is used to filter out the results
-
 '''
+
 def get_apps_version_time_dict(marathon_endpoint):
-	#r = requests.get("http://" + marathon_endpoint + "/v2/groups")
-	r = requests.get("http://localhost:8000/sample1.json")
+	r = requests.get("http://" + marathon_endpoint + "/v2/groups")
+	#r = requests.get("http://localhost:8000/sample1.json")
 	data = json.loads(r.content) 
 	group_collection = []
 
- 	'''
+ 	''' 
+ 	# To consider Marathon Applications as well
  	if(only_use_groups==False):
 		for app in data["apps"]:
 			used_ports.append(app["ports"][0])
 	'''
-	for group in data["groups"]:
+
+	for group in data["groups"]: # For each Group of Application Groups
 			group_def = {}
-			group_name = group["id"]
+			group_name = group["id"] 
 			print "group : " + str(group_name)
-			for each_group in group["groups"]:
-				if (len(each_group["apps"])>0):
-					group_version = each_group["apps"][0]["version"]
-					group_label = each_group["apps"][0]["labels"]
-					if len(group_label) > 0:
-						group_label_env = str(group_label[avoid_label_key])
-						if(group_label_env == avoid_label_value):
-							continue
+
+			for each_group in group["groups"]: # Each Application Group 
+				if (len(each_group["apps"])>0): # If there are apps in the Group
+					group_version = each_group["apps"][0]["version"] # Get the latest version of the App
+					group_label = each_group["apps"][0]["labels"] # Get the Labels declared fo the App
+					if len(group_label) > 0: # If there are indeed any labels
+						group_label_env = str(group_label[avoid_label_key]) # Get the Label Value for the key that needs to be ignored
+						if(group_label_env == avoid_label_value): # If the value of the Key matches with the one that needs to be ignored
+							continue # Don't add it to the removal list
 					
 					print 'group_version : ' + str(group_version)
-					group_def[group_name] = group_version
+					group_def[group_name] = group_version  # Add the Group Name and the Version (Time stamp) to the removal list
 			
 			if(len(group_def)!=0):
-				group_collection.append(group_def)
+				group_collection.append(group_def) # Add to the collection of all Groups that are deployed on Marathon, except the ones which are to be ignored
 						
 
 	return group_collection
@@ -58,37 +71,46 @@ def get_apps_version_time_dict(marathon_endpoint):
 Find Old apps on which actions can be taken (removed, archived, marked for deletion etc.)
 Only checks for non-PROD applications and application groups. 
 '''
+
 def is_old_app(date, days_filter=None, hours_filter=None):
 
 	marked_for_deletion = True
 	not_marked_for_deletion = False
 
 	
-	format = "%Y-%m-%dT%H:%M:%S.%fZ" #2015-12-10T02:41:59.588Z - sample time
-	d = datetime.datetime.strptime(date, format)
-	current_day = datetime.datetime.today()
-	current_day_UTC = datetime.datetime.now(timezone('UTC'))
-	d_utc = d.replace(tzinfo=timezone('UTC'))
+	format = "%Y-%m-%dT%H:%M:%S.%fZ" 
+	d = datetime.datetime.strptime(date, format) # Format the Version timestamp in the Marathon Application JSON
+
+	current_day_UTC = datetime.datetime.now(timezone('UTC')) # # Get the current Time in UTC Format
+	
+	d_utc = d.replace(tzinfo=timezone('UTC')) # Get the Version timestamp in the UTC Format
 		
-	date_diff = current_day_UTC - d_utc
+	date_diff = current_day_UTC - d_utc # Find the difference in time 
 
-	'''Check Filter'''
-	if(days_filter != None):
-		'''If the difference in dates exceeds the day filter value'''
-		if(date_diff.days>=days_filter): 
+	
+	if(days_filter != None): # Check if the Days filter is set in the command line args
+		if(date_diff.days>=days_filter):  		#If the difference in dates exceeds the day filter value
 			return marked_for_deletion
-	elif(hours_filter != None):
-		'''Find difference in number of hours'''
+	elif(hours_filter != None): # Check if the Hours filter is set in the command line args
+		# Find difference in number of hours
 		total_difference_in_hours = date_diff.days * 24 + (date_diff.seconds / 60 / 60)
-		'''If the difference in dates exceeds the hour filter value'''
-		if(total_difference_in_hours>=hours_filter):
+	
+		if(total_difference_in_hours>=hours_filter): # If the difference in dates exceeds the hour filter value'''
 			return marked_for_deletion
 
+	# If it does not match with days or hour filter, then its NOT marked for deletion
 	return not_marked_for_deletion
 
 
+
+'''
+Delete Old Applications that are marked for deletion based on the filter
+conditions - Days or Hours
+'''
+
 def delete_old_apps(marathon_endpoint, days_filter=None, hours_filter=None):
-	apps_map = get_apps_version_time_dict(marathon_endpoint)
+	# Get list of all app groups on Marathon except the ones that are configured to be ignored
+	apps_map = get_apps_version_time_dict(marathon_endpoint) 
 	for item in apps_map:
 		app_grp_name = item.keys()[0]
 		app_grp_version_timestamp = item[app_grp_name]
@@ -102,6 +124,11 @@ def delete_old_apps(marathon_endpoint, days_filter=None, hours_filter=None):
 			print "Old App. NOT Marked for Deletion"
 
 
+'''
+Save the Marathon Application deployment manifest for a given
+application group Name
+'''
+
 def save_app(marathon_endpoint, app_grp_name):
 	r = requests.get("http://" + marathon_endpoint + "/v2/groups/"+ app_grp_name)
 	#print r.content
@@ -111,6 +138,9 @@ def save_app(marathon_endpoint, app_grp_name):
 	return r.content
 
 
+'''
+Issue delete request to Marathon endpoint for a given application group
+'''
 
 def send_appgrp_delete_request(marathon_endpoint, appgrp):
 	delete_url = "http://" + str(marathon_endpoint) + "/v2/groups"+ appgrp
@@ -143,8 +173,6 @@ if __name__ == '__main__':
                    help='ignore Applications, and only consider Application Groups')'''
 		
 	args = parser.parse_args()
-
-	
 
 	only_use_groups = True
 	avoid_label_key = args.filterkey
